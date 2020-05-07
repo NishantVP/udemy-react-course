fab37d488487d9eafe3bf423b154e2e924ec2735
Nishant <nishant1914@gmail.com>
2020-05-06 18:23:34 -0700

    lecture 45

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hi, I'm a React Developer</h1>
-      </div>
-    );
+
+    state = {
+        persons: [
+            { name: "Nis", age: 29 },
+            { name: "Kshitja", age: 22},
+            { name: "Sarika", age: 29}
+        ],
+        otherState: 'some value'
+    };
+
+
+    switchNameHandler = () => {
+
+        // console.log("was clicked!");
+        this.state.persons[0].name = "Nishant";
+
+        this.setState({
+
+            persons: [
+                { name: "Nishant", age: 29 },
+                { name: "Kshitja", age: 22},
+                { name: "Sarika", age: 29}
+            ],
+
+        })
+
+    };
+
+
+    render() {
+      return (
+
+          <div className="App">
+
+              <h1> Hi, I'm a React Developer</h1>
+              <p>This is really working!</p>
+
+              <button onClick={this.switchNameHandler}> Switch Name </button>
+
+              <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+              <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} > My Hobbies: Racing </Person>
+              <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
+          </div>
+      );
+
+      // return (
+      //     React.createElement('div', {className : "App"},
+      //         React.createElement( 'h1', null, 'Hi, I\'m a React Developer'))
+      // );
+
   }
 }
+
+
 
 export default App;
